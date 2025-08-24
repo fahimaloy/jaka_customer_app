afe7ffb46fc467ffcf16e9247f2939390e1118c5
Fahim Ahmed <75910549+fahimaloy@users.noreply.github.com>
2025-08-24 20:03:49 +0600

    Add pinia store with persisted state

--- restaurant-kiosk/src/main.ts
+++ restaurant-kiosk/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import persisted from 'pinia-plugin-persistedstate'
 import './main.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+pinia.use(persisted)
+app.use(pinia)
+
+app.mount('#app')
